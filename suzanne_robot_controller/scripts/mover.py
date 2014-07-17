3496b00862cb20c4847019beae895bccb7549cb1
Benjamin Brieber <bbrieber@cs.uni-bremen.de>
2014-07-17 11:00:45 +0200

    updated controller

---
+++ suzanne_robot_controller/scripts/mover.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import random
+
+
+from geometry_msgs.msg import Twist
+
+
+def mover():
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    rospy.init_node('mover', anonymous=True)
+    r = rospy.Rate(5) # 10hz
+    #random.seed()
+    rospy.loginfo("Intializing controll module")
+    while not rospy.is_shutdown():
+        cmd = Twist()
+        cmd.linear.x = 1
+        #cmd.angular.z = random.randint(-1,1)
+        pub.publish(cmd)
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        mover()
+    except rospy.ROSInterruptException: pass
